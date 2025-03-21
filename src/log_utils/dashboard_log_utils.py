2e93289062d34de04b658a2bfface7c06c04d77e
Alexandra Abbas <32793463+alexandraabbas@users.noreply.github.com>
2025-03-20 20:26:01 -0700

    Cache functions

--- src/log_utils/dashboard_log_utils.py
+++ src/log_utils/dashboard_log_utils.py
@@ -6,6 +6,14 @@ from inspect_evals_dashboard_schema import DashboardLog
 from src.config import EvaluationConfig
 
 
+def dashboard_log_hash_func(obj: DashboardLog) -> str:
+    # Streamlit raises a UnhashableParamError since it does not know
+    # how to hash DashboardLog. Here we define a custom hash function
+    # that uses run_id to uniquely identify the object.
+    return obj.eval.run_id
+
+
+@st.cache_data(hash_funcs={DashboardLog: dashboard_log_hash_func, EvalScore: id})
 def get_scorer_by_name(log: DashboardLog, scorer_name: str) -> EvalScore:
     try:
         return next(score for score in log.results.scores if score.name == scorer_name)
@@ -14,6 +22,7 @@ def get_scorer_by_name(log: DashboardLog, scorer_name: str) -> EvalScore:
         return log.results.scores[0]
 
 
+@st.cache_data(hash_funcs={EvaluationConfig: id})
 def read_default_values_from_configs(eval_configs: list[EvaluationConfig]) -> dict[str, dict[str, str]]:
     default_values: dict[str, dict[str, str]] = {}
     for config in eval_configs:
@@ -22,13 +31,6 @@ def read_default_values_from_configs(eval_configs: list[EvaluationConfig]) -> di
             "default_metric": config.default_metric
         }
     return default_values
-
-
-def dashboard_log_hash_func(obj: DashboardLog) -> str:
-    # Streamlit raises a UnhashableParamError since it does not know
-    # how to hash DashboardLog. Here we define a custom hash function
-    # that uses run_id to uniquely identify the object.
-    return obj.eval.run_id
 
 
 @st.cache_data(hash_funcs={DashboardLog: dashboard_log_hash_func})
