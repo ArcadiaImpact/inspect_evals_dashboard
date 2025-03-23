38c8b121dde7d646d4ceb5c0b77556fa8efd3187
Alexander Putilin <avp-13@yandex.ru>
2025-03-23 21:41:21 +0000

    Add a test for load_evaluation_logs()

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["STREAMLIT_ENV"] = "test"
--- tests/test_load_eval_logs.py
+++ tests/test_load_eval_logs.py
@@ -1,19 +1,31 @@
 import pytest
 from pytest import MonkeyPatch
 
-from src.log_utils.load_eval_logs import get_log_paths
+from src.log_utils.load_eval_logs import get_log_paths, load_evaluation_logs
 from src.config import load_config
 
 def test_get_log_paths():
     with MonkeyPatch.context() as mp:
-        mp.setenv('STREAMLIT_ENV', 'test')
-
         config = load_config()
 
         group_config = config.agents
 
         log_paths = get_log_paths(group_config)
-        print("Log paths", log_paths)
 
         assert len(log_paths) > 0
-        assert log_paths[0].startswith('test/data/')
+        assert log_paths[0].startswith('tests/data/')
+
+
+def test_load_evaluation_logs():
+    with MonkeyPatch.context() as mp:
+        config = load_config()
+        group_config = config.agents
+
+        eval_logs = load_evaluation_logs(get_log_paths(group_config))
+
+        assert len(eval_logs) > 0
+        
+        log = eval_logs[0]
+        assert log.task_metadata.name == "pubmedqa"
+        assert log.cost_estimates["total"] > 0
+        assert log.eval is not None
