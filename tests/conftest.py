c85b5b1c6d853576038c940832a4936e11d35a5b
Alexander Putilin <mimimitor@gmail.com>
2025-03-24 23:05:46 +0000

    Update tests/conftest.py
    Co-authored-by: Alexandra Abbas <32793463+alexandraabbas@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from src.config import load_config
 from src.log_utils.load_eval_logs import get_log_paths, load_evaluation_logs
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def eval_logs():
     group_config = load_config().agents
     return load_evaluation_logs(get_log_paths(group_config))
